3239c42f90e3e3e7f88ed5d0a9bf1e94022311e8
bart.kuijper <bart.kuijper@everbridge.com>
2023-02-11 20:34:11 +0100

    comments'

--- src/app/components/dock/dock-item/dock-item.component.ts
+++ src/app/components/dock/dock-item/dock-item.component.ts
@@ -16,6 +16,7 @@ export class DockItemComponent {
     this.startBounce$.next('')
   }
 
+  // exstensible
   bounce$ = merge(this.startBounce$).pipe(
     concatMap(() => this.bouncing$)
   )
@@ -25,27 +26,26 @@ export class DockItemComponent {
   bouncing$ = defer(() => {
     const timeSinceBounceStart = Date.now()
     return interval(0, animationFrameScheduler).pipe(
-      // non-deterministic frames, in other words as many 'ticks' as your computer can give you on the 'animation frame'
+      // non-deterministic 'temporal set' of frames, in other words as many 'ticks' as your computer can give you on the 'animation frame' is available.
       map(() => Date.now() - timeSinceBounceStart),
-      tap(x => console.log('x', x)),
-      // turn whatever your cocmputer gives into deterministic 'frames' so that animation travel similar distance over time on all CPU speeds
+      // turn whatever your CPU gives into deterministic 'frames' so that animation value (soon degrees) is temporaly smooth but precise.
       map(t => t / 100),
       // same amount of frames but lower values for each
       // conceptually now best think of rotation in degrees and how a higher degree can be mapped to a longer leg of a triangle
       map(d => d + 4.7),
-      tap(x => console.log('a', x, 'and', Date.now() - timeSinceBounceStart)),
       // with a offset value to assure the starting is at the same point as where the icon is
       map(d => Math.sin(d) + 1),
-      // now we are in triangle land (we do + 1 to assure only possitive values) imagine the right leg of a triangles length mapped to height
+      // now we are in triangle land (we do + 1 to assure only possitive values) imagine the right leg of a triangle length from 1 to 2 (due to +1) mapped to height
       map(h => h * 16),
-      // this represent the height we go out 32 pixel roughly (16 * 2)
+      // this represent the height we go out 32 pixel roughly (16 * 2) as the sinus + 1 gives us values between 0 and 2
       scan((p: {h: number, r: number}, ch: number) => {
         return ({h: ch, r: (p.r % 2  == 0) ? ch > p.h ? p.r : p.r + 1 : ch < p.h ? p.r : p.r + 1  })
       }, {h: 0, r: 0}),
       // scan allows us to persist some state during the lifecycle of the observable the r repsents cycles. By looking if values go up (until they don't) and vice versa
+      // everytime a value is down or up from its former trend we + 1 and by checking on even oneven we create a nice cycle
       // we can determine when to stop the animation using the takeWhile
       takeWhile(({r}) =>  r < 6),
-      // takeWhile completes when a certain value is reached, here we basically say do 2 hops
+      // takeWhile completes when a certain value is reached, here we basically say do 3 hops (or 6 ends reached)
       map(({h}) => `${h}px`),
       // final mapping to height of that icon relative to its position
       )
