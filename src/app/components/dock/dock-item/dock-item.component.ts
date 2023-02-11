5897650f0b7a0c1b9e75553cd022af9841f63edb
bart.kuijper <bart.kuijper@everbridge.com>
2023-02-11 20:46:14 +0100

    defer as well

--- src/app/components/dock/dock-item/dock-item.component.ts
+++ src/app/components/dock/dock-item/dock-item.component.ts
@@ -24,6 +24,7 @@ export class DockItemComponent {
 
 
   bouncing$ = defer(() => {
+    // defer postpones the evaluation of the observable until it get subscribed. This is useful when wanting to evaluate time from the moment the concatmap subs to bounces.
     const timeSinceBounceStart = Date.now()
     return interval(0, animationFrameScheduler).pipe(
       // non-deterministic 'temporal set' of frames, in other words as many 'ticks' as your computer can give you when the 'animation frame' is available.
